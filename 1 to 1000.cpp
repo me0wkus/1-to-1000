8be2c942d5427948ea112d3e72b4e41c46e624e4
me0wkus <118027059+me0wkus@users.noreply.github.com>
2022-11-12 21:11:59 +0300

    Add files via upload

---
+++ 1 to 1000.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int i;
+	int s = 0;
+
+	for (i = 1; i <= 1000; i++) { //Если ставить тут (;), то оператор заканчивает подсчёт и затем s = s + i добавляет ещё 1, и в ответе получается 1001//
+
+		s = s + i;
+
+	}
+
+	cout << "Summ of a numbers from 1 to 1000 is: " << s << endl;
+	return 0;
+
+}
